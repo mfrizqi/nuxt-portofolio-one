5f7b7d17c409160cc57b0a8b05edbc1b8310f787
mfrizqi <muhammadfajarrizqi@gmail.com>
2021-07-12 14:27:14 +0700

    Add files via upload

---
+++ _nuxt/static/1626074587/login/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-portofolio-one\u002F_nuxt\u002Fstatic\u002F1626074587",layout:"default",error:a,serverRendered:true,routePath:"\u002Flogin",config:{_app:{basePath:"\u002Fnuxt-portofolio-one\u002F",assetsPath:"\u002Fnuxt-portofolio-one\u002F_nuxt\u002F",cdnURL:a}}}}(null));
